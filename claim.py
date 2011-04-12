a520f1b11e9db33fa558abeaa1a44f90e55df755
Christopher L. Mccoy Jr <cmccoy@zerowing.ccs.neu.edu>
2011-04-11 23:08:18 -0400

    Claim object stub

---
+++ claim.py
@@ -0,0 +1,12 @@
+# vim: set expandtab:ts=8:sw=4:softtabstop=4:smarttab
+#!/usr/bin/env python
+
+class Claim:
+    """
+    Represents a claim
+    """
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
